eba7b884a8e52e84545fcf361e531ac29a2d29fd
Denys <denys@Denyss-MacBook-Pro.local>
2023-11-01 13:53:39 +0200

    Added Routes, Links and Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategiry from "./Pages/ShopCategiry";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategiry category="men" />} />
+          <Route path="/womens" element={<ShopCategiry category="women" />} />
+          <Route path="/kids" element={<ShopCategiry category="kid" />} />
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
