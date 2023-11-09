ff45d71102cc19f67b549ee0b3f2e6dc6b60e843
Denys <denys@Denyss-MacBook-Pro.local>
2023-11-09 19:04:42 +0200

    ShopCategory page is ready

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
-import ShopCategiry from "./Pages/ShopCategiry";
+import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from './Components/Assets/banner_mens.png'
+import woman_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategiry category="men" />} />
-          <Route path="/womens" element={<ShopCategiry category="women" />} />
-          <Route path="/kids" element={<ShopCategiry category="kid" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={woman_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
           <Route path="/product" element={<Product/>}>
             <Route path=":productId" element={<Product/>} />
           </Route>
